0f8433790e83ef5fc6d24d8536d292eccf79a661
joelouie222 <joelouie222@gmail.com>
2021-04-07 10:24:00 -0500

    Add project files.

---
+++ Lab17Stack/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Lab17Stack
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+
+            IntStack tenInts = new IntStack(10);
+            bool mt = tenInts.IsEmpty();  // true
+            bool full = tenInts.IsFull(); // false
+
+            tenInts.Push(123);
+            mt   = tenInts.IsEmpty(); // false
+            full = tenInts.IsFull (); // false
+
+            tenInts.Push(456);
+            tenInts.Push(-1);
+
+            int v1 = tenInts.Pop(); // -1
+            int v2 = tenInts.Pop(); // 456
+            tenInts.Pop();
+
+            tenInts.Push(789);
+            tenInts.Push(111);
+            tenInts.Push(222);
+            tenInts.Push(333);
+            tenInts.Push(444);
+            tenInts.Push(555);
+            tenInts.Push(666);
+            tenInts.Push(777);
+            full = tenInts.IsFull(); // false
+
+            tenInts.Push(888);
+            full = tenInts.IsFull(); // true
+
+            tenInts.Push(789);
+            tenInts.Push(789);
+
+            //String Stack
+            StringStack reverseString = new StringStack(10);
+            reverseString.Push("The ");
+            reverseString.Push("car ");
+            reverseString.Push("is ");
+            reverseString.Push("a ");
+            reverseString.Push("racecar ");
+            reverseString.Push("!");
+            Console.Write(reverseString.Pop());
+            Console.Write(reverseString.Pop());
+            Console.Write(reverseString.Pop());
+            Console.Write(reverseString.Pop());
+            Console.Write(reverseString.Pop());
+            Console.Write(reverseString.Pop());
+
+            //CharStack
+            CharStack helloWorld = new CharStack(11);
+            helloWorld.Push('H');
+            helloWorld.Push('e');
+            helloWorld.Push('l');
+            helloWorld.Push('l');
+            helloWorld.Push('o');
+            helloWorld.Push(' ');
+            helloWorld.Push('W');
+            helloWorld.Push('o');
+            helloWorld.Push('r');
+            helloWorld.Push('l');
+            helloWorld.Push('d');
+            Console.WriteLine();
+            while (!helloWorld.IsEmpty())
+                Console.Write(helloWorld.Pop());
+
+
+            //GenericStack
+            Stack<double> newStack = new Stack<double>(5);
+            newStack.Push(2.15485489);
+            newStack.Push(3.34741);
+            newStack.Push(1.43);
+            newStack.Pop();
+            Console.WriteLine();
+            while (!newStack.IsEmpty())
+                Console.WriteLine(newStack.Pop());
+
+
+            Console.WriteLine();
+        }
+   }
+}
---
+++ Lab17Stack/Stack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab17Stack
+{
+    class Stack<T>
+    {
+        //   F i e l d s   &   P r o p e r t i e s
+
+        private int _size;       // initially 0
+        private T[] _stack;      // initially null
+        private int _topOfStack; // initially 0
+
+
+        //   C o n s t r u c t o r s
+
+        public Stack(int size)
+        {
+            _size = size;
+            _stack = new T[size];
+        }
+
+
+        //   M e t h o d s
+
+        public bool IsEmpty()
+        {
+            if (_topOfStack > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool IsFull()
+        {
+            if (_topOfStack >= _size)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public T Pop()
+        {
+            _topOfStack--;
+            return _stack[_topOfStack];
+        }
+
+        public void Push(T valueToPush)
+        {
+            if (!IsFull())
+            {
+                _stack[_topOfStack] = valueToPush;
+                _topOfStack++;
+            }
+        }
+
+    }
+}
